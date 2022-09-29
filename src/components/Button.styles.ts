ae65aa9812bc6a14464e85969a8942af8e29d8ac
Michel Fonseca <michelbeserradafonseca@gmail.com>
2022-09-29 15:08:01 -0300

    Colors and Fonts added

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,25 +6,14 @@ interface ButtonContainerProps {
     variant: ButtonVariant;
 }
 
-const buttonVariants = { 
-    primary: 'purple',
-    secondary: 'orange',
-    danger: 'red',
-    success: 'green',
-}
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 50px;
     border-radius: 4px;
     border: 0;
     margin: 8px;
-    background-color: ${props => props.theme.primary};
+    background-color: ${props => props.theme['gray-500']};
     color: ${props => props.theme.white}
 
-    /* ${props => {
-        return css`
-        background-color: ${buttonVariants[props.variant]}
-        `
-    }} */
+    
 `
