36b45f2a1e43fe910bf6af8d293bfa602c9cc67d
Michel Fonseca <michelbeserradafonseca@gmail.com>
2022-09-30 13:52:12 -0300

    Status component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status} from "./styles";
 
 export function History(){
     return (
@@ -20,19 +20,25 @@ export function History(){
                             <td>Tasks</td>
                             <td>20 minutes</td>
                             <td>About 2 mounths</td>
-                            <td>Done</td>
+                            <td>
+                                <Status statusColor="green">Done</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tasks</td>
                             <td>20 minutes</td>
                             <td>About 2 mounths</td>
-                            <td>In progress</td>
+                            <td>
+                                <Status statusColor="yellow">In Progress</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>Tasks</td>
                             <td>20 minutes</td>
                             <td>About 2 mounths</td>
-                            <td>Interrupted</td>
+                            <td>
+                                <Status statusColor="red">Interrupted</Status>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
