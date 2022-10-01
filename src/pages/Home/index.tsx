9b49a93ce0de025cc783ebe6b5b0cfd1df97b29e
Michel Fonseca <michelbeserradafonseca@gmail.com>
2022-10-01 06:08:24 -0300

    form reset

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -16,7 +16,7 @@ type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>;
  
 export function Home(){
 
-    const { register, handleSubmit, watch} = useForm<NewCycleFormData>({
+    const { register, handleSubmit, watch, reset} = useForm<NewCycleFormData>({
         resolver: zodResolver(newCycleFormValidationSchema),
         defaultValues: {
             task: '',
@@ -30,6 +30,7 @@ export function Home(){
 
     function handleCreateNewCycle(data: NewCycleFormData) {
         console.log(data);
+        reset();
     };
 
     const task = watch('task');
